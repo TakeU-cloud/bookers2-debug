1fa31527229bde83045fb814fe1f1667cc67eb11
Takeshi Uto <ec2-user@ip-172-31-40-226.ap-northeast-1.compute.internal>
2023-05-19 01:12:15 +0000

    delete commentout from migration files

--- db/migrate/20230513100905_create_relationships.rb
+++ db/migrate/20230513100905_create_relationships.rb
@@ -8,17 +8,3 @@ class CreateRelationships < ActiveRecord::Migration[6.1]
     end
   end
 end
-
-# class CreateRelationships < ActiveRecord::Migration[6.1]
-#   def change
-#     create_table :relationships do |t|
-#       t.bigint :follower_id
-#       t.bigint :followed_id
-
-#       t.timestamps
-#     end
-#     add_index :relationships, [:follower_id, :followed_id], unique: true
-#     add_foreign_key :relationships, :users, column: :follower_id, on_delete: :cascade
-#     add_foreign_key :relationships, :users, column: :followed_id, on_delete: :cascade
-#   end
-# end
--- db/migrate/20230515020825_create_direct_messages.rb
+++ db/migrate/20230515020825_create_direct_messages.rb
@@ -9,18 +9,3 @@ class CreateDirectMessages < ActiveRecord::Migration[6.1]
     end
   end
 end
-
-# class CreateDirectMessages < ActiveRecord::Migration[6.1]
-#   def change
-#     create_table :direct_messages do |t|
-#       t.bigint :sender_id
-#       t.bigint :receiver_id
-#       t.string :content
-
-#       t.timestamps
-#     end
-#     add_index :direct_messages, [:sender_id, :receiver_id]
-#     add_foreign_key :direct_messages, :users, column: :sender_id, on_delete: :cascade
-#     add_foreign_key :direct_messages, :users, column: :receiver_id, on_delete: :cascade
-#   end
-# end
