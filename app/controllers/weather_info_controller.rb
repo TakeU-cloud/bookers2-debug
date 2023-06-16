7430ebc9e91af4563c8a931e87e0d2717a910735
Takeshi Uto <uto@kotetsu-japan.co.jp>
2023-06-16 03:10:35 +0000

    お天気情報にTMDBの映画情報も出してみる

--- app/controllers/weather_info_controller.rb
+++ app/controllers/weather_info_controller.rb
@@ -5,6 +5,7 @@ class WeatherInfoController < ApplicationController
   def show
     city_name = params[:city_name]
     api_key = ENV["OPEN_WEATHER_API_KEY"]
+    api_key2 = ENV["TMDB_API_KEY"]
 
     if city_name.present?
       begin
@@ -19,6 +20,33 @@ class WeatherInfoController < ApplicationController
             speed: data['wind']['speed'],
             icon_url: "http://openweathermap.org/img/w/#{data['weather'][0]['icon']}.png"
           }
+        else
+          @error_message = "API Error: #{data['message']}"
+        end
+      rescue Faraday::ConnectionFailed => e
+        @error_message = "Connection failed: #{e.message}"
+      rescue JSON::ParserError => e
+        @error_message = "Parsing failed: #{e.message}"
+      rescue URI::InvalidURIError => e
+        @error_message = "Invalid URI: #{e.message}"
+      rescue Faraday::ClientError => e
+        @error_message = "Client error: #{e.message}"
+      end
+      # city_nameが入ったら、ついでにTMDBの映画のポスター画像3枚とタイトルを取得して、@moviesに格納する
+      begin
+        response = Faraday.get "https://api.themoviedb.org/3/movie/now_playing?api_key=#{api_key2}&language=ja&region=JP&page=1"
+        data = JSON.parse(response.body)
+        if response.status == 200
+          @movies = []
+          count = 0
+          data['results'].each do |movie|
+            @movies << {
+              title: movie['title'],
+              poster_path: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}"
+            }
+            count += 1
+            break if count == 3
+          end
         else
           @error_message = "API Error: #{data['message']}"
         end
